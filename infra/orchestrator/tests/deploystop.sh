bb712e99765743bcf4e9bd849c89a3ac7ec025e0
Hanz <hi@hanz.dev>
2025-10-05 19:40:49 +0700

    test: More instances

--- infra/orchestrator/tests/deploystop.sh
+++ infra/orchestrator/tests/deploystop.sh
@@ -3,12 +3,25 @@ echo "Deploying"
 
 for i in $(seq 1 100); do
 	curl -s -X POST "http://localhost:8080/deploy/$i" &
-	sleep 0.1
 done
+
+sleep 30
+
+for i in $(seq 101 200); do
+	curl -s -X POST "http://localhost:8080/deploy/$i" &
+done
+
+sleep 30
+
+for i in $(seq 201 300); do
+	curl -s -X POST "http://localhost:8080/deploy/$i" &
+done
+
+sleep 30
 
 echo
 echo "Stopping"
 
-for i in $(seq 1 100); do
+for i in $(seq 1 300); do
 	curl -s -X POST "http://localhost:8080/stop/$i"
 done
