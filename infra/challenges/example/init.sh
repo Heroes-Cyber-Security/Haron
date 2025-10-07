9b99d8d52cc4bf72ab7a6e525061ccc41e95741a
Hanz <hi@hanz.dev>
2025-10-07 11:20:35 +0700

    feat(example): Added init for Setup.sol

--- infra/challenges/example/init.sh
+++ infra/challenges/example/init.sh
@@ -1,1 +1,20 @@
 forge init --no-git --force .
+
+# Cleanup
+rm src/Counter.sol
+rm test/Counter.t.sol
+rm script/Counter.s.sol
+
+# Setup.sol
+cat <<EOF > src/Setup.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Setup {
+	constructor() {}
+
+	function isSolved() view returns (bool) {
+		return true;
+	}
+}
+EOF
