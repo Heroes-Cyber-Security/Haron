11207b405c04290175abf79da2dcaea1f6867372
Hanz <hi@hanz.dev>
2025-10-10 11:21:11 +0700

    feat(worker): Added job initialization report generation

--- infra/worker/src/app.py
+++ infra/worker/src/app.py
@@ -1,7 +1,7 @@
-import zipfile
+from copy import deepcopy
 from gevent import monkey; monkey.patch_all()
 from bottle import get, post, run, request, HTTPError
-import subprocess, os, uuid
+import subprocess, os, uuid, venv, json, zipfile
 
 jobs = {}
 initialized = set()
@@ -10,11 +10,19 @@ class Job(object):
 	uid: str
 	task: str
 	anvil_endpoint: str
+	report: dict = {}
 
 	def __init__(self, uid, task, anvil_endpoint):
 		self.uid = uid
 		self.task = task
 		self.anvil_endpoint = anvil_endpoint
+
+def generate_report(cwd):
+	result = subprocess.run(f"{cwd}.venv/bin/python {cwd}chal.py", shell=True, cwd=cwd)
+	content = result.stdout.decode('latin-1')
+	data = json.loads(content)
+
+	return data
 
 @post("/stop/:jobid")
 def stop(jobid):
@@ -27,6 +35,7 @@ def delegate(h):
 	/delegate/:h?anvil_endpoint=
 	"""
 	uid = str(uuid.uuid4())
+	# TODO: Use programmatic path instead of str concats
 	cwd = f"/home/ctf/cache/{h}/"
 
 	jobs[uid] = Job(uid, h, request.query['anvil_endpoint'])
@@ -34,8 +43,11 @@ def delegate(h):
 	if h not in initialized:
 		with zipfile.ZipFile(cwd + h + ".zip", "r") as zr:
 			zr.extractall(cwd)
-		subprocess.run(f"pip install -r {cwd}requirements.txt", shell=True, cwd=cwd)
+		venv.create(cwd + ".venv", clear=True, with_pip=True, symlinks=True)
+		subprocess.run(f"{cwd}.venv/bin/pip install -r {cwd}requirements.txt", shell=True, cwd=cwd)
 		initialized.add(h)
+	
+	jobs[uid].report = deepcopy(generate_report(cwd))
 
 	return uid
 
