d6dd34db6611172d62666aef8e73850661a0b642
hanz <hi@hanz.dev>
2026-02-27 22:42:47 +0700

    fix(example): add missing is_solved function to chal.py

--- infra/challenges/example/chal.py
+++ infra/challenges/example/chal.py
@@ -1,35 +1,47 @@
 from typing import Dict, Optional, Tuple
 
+
 def precompile() -> Dict:
-	"""
-	EXPERIMENTAL
-	"""
-	return dict()
+    """
+    EXPERIMENTAL
+    """
+    return dict()
+
 
 def genesis() -> Optional[Dict]:
-	"""
-	Returns a genesis.json file
-	"""
-	return None
+    """
+    Returns a genesis.json file
+    """
+    return None
+
 
 def on_new_block():
-	"""
-	Triggered every time a new block has been mined
-	"""
-	pass
+    """
+    Triggered every time a new block has been mined
+    """
+    pass
+
 
 def on_incoming_tx() -> Tuple[int, str]:
-	"""
-	Triggered every time a new tx is proposed to the mempool
+    """
+    Triggered every time a new tx is proposed to the mempool
 
-	Returns:
-	 - int: HTTP status code, with 4xx dropping the incoming tx
-	 - str: Error message when tx is dropped
-	"""
-	return 200, ""
+    Returns:
+     - int: HTTP status code, with 4xx dropping the incoming tx
+     - str: Error message when tx is dropped
+    """
+    return 200, ""
+
 
 def on_tx():
-	"""
-	Triggered every time a transaction has been included in a block
-	"""
-	pass
+    """
+    Triggered every time a transaction has been included in a block
+    """
+    pass
+
+
+def is_solved() -> bool:
+    """
+    Returns True if the challenge has been solved
+    """
+    return True
