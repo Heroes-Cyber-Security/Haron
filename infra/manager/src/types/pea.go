2ca9e8e829c8bceae8c3de172536fb0032b93165
hanz <hi@hanz.dev>
2026-02-28 05:31:44 +0700

    feat(manager): add ChainIds to Pea type with endpoint helpers

--- infra/manager/src/types/pea.go
+++ infra/manager/src/types/pea.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 type Pea struct {
 	Id               string
 	ChallengeHash    string
@@ -7,8 +9,24 @@ type Pea struct {
 	WorkerJobUid     string
 	SetupAddress     string
 	PlayerPrivateKey string
+	ChainIds         []uint64
 }
 
 func (pea Pea) GetAnvilEndpoint() string {
-	return "http://orchestrator:8080/anvil/" + pea.Id
+	if len(pea.ChainIds) == 0 {
+		return "http://orchestrator:8080/anvil/" + pea.Id
+	}
+	return "http://orchestrator:8080/anvil/" + pea.Id + "/" + formatChainId(pea.ChainIds[0])
+}
+
+func (pea Pea) GetAnvilEndpoints() []string {
+	endpoints := make([]string, len(pea.ChainIds))
+	for i, chainId := range pea.ChainIds {
+		endpoints[i] = "http://orchestrator:8080/anvil/" + pea.Id + "/" + formatChainId(chainId)
+	}
+	return endpoints
+}
+
+func formatChainId(chainId uint64) string {
+	return fmt.Sprintf("%d", chainId)
 }
