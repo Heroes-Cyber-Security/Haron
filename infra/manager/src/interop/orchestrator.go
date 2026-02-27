4973cf0cca2fbc707ef27f6c5aea6fcee7f2e96a
hanz <hi@hanz.dev>
2026-02-28 05:32:10 +0700

    feat(manager): pass chainIds to orchestrator deploy

--- infra/manager/src/interop/orchestrator.go
+++ infra/manager/src/interop/orchestrator.go
@@ -2,6 +2,9 @@ package interop
 
 import (
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"blockchain.hanz.dev/manager/types"
 	"github.com/ddo/rq"
@@ -10,7 +13,16 @@ import (
 var ORCHESTRATOR_BASE = "http://orchestrator:8080"
 
 func Deploy(pea types.Pea) {
-	r := rq.Post(ORCHESTRATOR_BASE + "/deploy/" + pea.Id)
+	chainIdsStr := ""
+	if len(pea.ChainIds) > 0 {
+		chainIds := make([]string, len(pea.ChainIds))
+		for i, id := range pea.ChainIds {
+			chainIds[i] = strconv.FormatUint(id, 10)
+		}
+		chainIdsStr = "?chains=" + url.QueryEscape(strings.Join(chainIds, ","))
+	}
+
+	r := rq.Post(ORCHESTRATOR_BASE + "/deploy/" + pea.Id + chainIdsStr)
 	req, err := r.ParseRequest()
 	if err != nil {
 		return
