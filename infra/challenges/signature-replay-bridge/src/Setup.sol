f4f05ccb41365d3ef59c4c3c998c04c1dad3068b
hanz <hi@hanz.dev>
2026-02-28 06:05:03 +0700

    feat(challenge): add Setup.sol wrapper for bridge

---
+++ infra/challenges/signature-replay-bridge/src/Setup.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Bridge.sol";
+import "./Token.sol";
+
+contract Setup {
+    IBridge public BRIDGE;
+    IERC20 public TOKEN;
+
+    constructor() {
+        TOKEN = new ReplayToken(1000000);
+        BRIDGE = new SignatureReplayBridge(address(TOKEN), msg.sender);
+        IReplayToken(TOKEN).mint(address(BRIDGE), 1000000 * 10 ** 18);
+    }
+
+    function getBridge() external view returns (address) {
+        return address(BRIDGE);
+    }
+
+    function getToken() external view returns (address) {
+        return address(TOKEN);
+    }
+}
